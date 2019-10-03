fde8ecd83364bc41c6a17be248635f1464fb7481
Hang Zhang <zhang.hang@rutgers.edu>
2019-10-03 11:44:40 -0700

    Question about grad update on non-active pass
    I am reading through the code. Why are you also adding grad to non-active path? Am I missing something?

--- search/modules/mix_op.py
+++ search/modules/mix_op.py
@@ -132,6 +132,7 @@ class MixedEdge(MyModule):
                         for k in range(len(candidate_ops)):
                             if k != active_id:
                                 out_k = candidate_ops[k](_x.data)
+                                # why adding grad to the non-active pash?
                             else:
                                 out_k = _output.data
                             grad_k = torch.sum(out_k * grad_output)
