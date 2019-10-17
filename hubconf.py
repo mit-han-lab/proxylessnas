4d8ba3a0299e5e202801573b8aebbbbe1b898e43
Lyken17 <ligeng@mit.edu>
2019-10-17 19:50:04 +0800

    prepare conf file for pytorch hubs.

---
+++ hubconf.py
@@ -0,0 +1,4 @@
+dependencies = ['torch', 'torchvision']
+
+from torch.hub import load_state_dict_from_url
+from .proxyless_nas.model_zoo import proxyless_cpu, proxyless_gpu, proxyless_mobile, proxyless_mobile_14
