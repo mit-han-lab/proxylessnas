0dbac4b2aa5c7abd51549d83b8b50c1746f1dd58
Ligeng Zhu <ligeng@mit.edu>
2020-05-04 18:28:58 -0400

    update torchvision model urls

--- training/net224x224/resnet.py
+++ training/net224x224/resnet.py
@@ -7,12 +7,13 @@ __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
            'resnet152']
 
 
+
 model_urls = {
-    'resnet18': 'https://download.pytorch.org/net224x224/resnet18-5c106cde.pth',
-    'resnet34': 'https://download.pytorch.org/net224x224/resnet34-333f7ec4.pth',
-    'resnet50': 'https://download.pytorch.org/net224x224/resnet50-19c8e357.pth',
-    'resnet101': 'https://download.pytorch.org/net224x224/resnet101-5d3b4d8f.pth',
-    'resnet152': 'https://download.pytorch.org/net224x224/resnet152-b121ed2d.pth',
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
 }
 
 
